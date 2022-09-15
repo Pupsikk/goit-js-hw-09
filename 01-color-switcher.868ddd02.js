32e187c63c057ec650bb4f52158fafd23cfb9535
Pupsikk <100074196+Pupsikk@users.noreply.github.com>
2022-09-15 20:07:17 +0000

    Deploying to gh-pages from @ Pupsikk/goit-js-hw-09@7eb59131338bcb79559b73162fd5e00d1f770a1e 🚀

---
+++ 01-color-switcher.868ddd02.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};let e=null;t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(function(){t.btnStop.disabled=!1,t.btnStart.disabled=!0,e=setInterval((()=>{let e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.body.style.backgroundColor=e}),1e3)})),t.btnStop.addEventListener("click",(function(){clearInterval(e),t.btnStop.disabled=!0,t.btnStart.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.868ddd02.js.map
